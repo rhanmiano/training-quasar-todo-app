4349f0a8dc975ef9501abd71084e93f12dd224d1
rhanmiano <rhanmiano29@gmail.com>
2019-07-27 23:58:23 +0800

    center layout

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,47 +1,48 @@
 <template>
     <div class="q-pa-md q-gutter-y-sm">
         <q-toolbar class="text-primary">
-        <q-btn flat round dense icon="menu" />
-        <q-toolbar-title>
-            Todo App
-        </q-toolbar-title>
-        <q-btn flat round dense icon="bar_chart" @click="$router.push('/chart')" />
-        <q-btn flat round dense icon="more_vert" />
+            <q-btn flat round dense icon="menu" />
+            <q-toolbar-title>
+                Todo App
+            </q-toolbar-title>
+            <q-btn flat round dense icon="bar_chart" @click="$router.push('/chart')" />
+            <q-btn flat round dense icon="more_vert" />
         </q-toolbar>
 
         <div class="q-pa-sm">
-            <div class="row items-center">
-                <div class="col-10">
-                    <q-input @keyup.enter="addTodo" v-model="newTodo" label="Task" />
-                </div>
-                <div class="col-2 vertical-middle text-center">
+            <div class="row items-center justify-center">
+                <div class="col-xs-12 col-sm-6 vertical-middle">
+                    <q-input class="q-mb-sm" @keyup.enter="addTodo"
+                    v-model="newTodo" label="Task" />
                     <q-btn @click="addTodo" :disabled="!newTodo"
-                    color="white" text-color="black" icon="send" round/>
+                    color="white" text-color="black" side>Add</q-btn>
                 </div>
             </div>
 
-        </div>
-
-        <div class="q-pa-sm">
-            <h6>Todos</h6>
-            <q-item clickable v-ripple v-for="(todo, i) in todos" v-bind:key="i">
-                <q-item-section avatar>
-                    <q-checkbox @click="!todo.completed" v-model="todo.completed" />
-                </q-item-section>
-                <q-item-section>
-                    <q-item-label :class="{'strike-through': todo.completed }">
-                        {{ todo.title }}
-                    </q-item-label>
-                </q-item-section>
-                <q-item-section side>
-                    <q-btn @click="deleteTodo(todo.id)" icon="delete"
-                    color="negative" round size="sm"></q-btn>
-                </q-item-section>
-            </q-item>
-        </div>
-
-        <div class="q-pa-sm">
-            <h6>Done Todos</h6>
+             <div class="row item-center justify-center">
+                <div class="col-xs-12 col-sm-6">
+                    <h6>Todos</h6>
+                    <q-item clickable v-ripple v-for="(todo, i) in todos" v-bind:key="i">
+                        <q-item-section avatar>
+                            <q-checkbox @click="!todo.completed" v-model="todo.completed" />
+                        </q-item-section>
+                    <q-item-section>
+                        <q-item-label :class="{'strike-through': todo.completed }">
+                            {{ todo.title }}
+                        </q-item-label>
+                    </q-item-section>
+                        <q-item-section side>
+                            <q-btn @click="deleteTodo(todo.id)" icon="delete"
+                            color="negative" round size="sm"></q-btn>
+                        </q-item-section>
+                    </q-item>
+                </div>
+            </div>
+            <div class="row item-center justify-center">
+                <div class="col-xs-12 col-sm-6">
+                    <h6>Done Todos</h6>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -79,7 +80,9 @@ export default {
       this.newTodo = '';
     },
     deleteTodo(id) {
-      this.todos.splice(id, 1);
+      console.log(id);
+      console.log(this.todos);
+    //   this.todos.splice(id, 1);
     },
     doneTodo() {
     //   this.todos.splice();
